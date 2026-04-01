baa4e3b8481f003eb6b1f321a1aa20c62e1ab043
jorgeamadosm2 <jorge_amadosm@outlook.es>
2026-04-01 12:47:11 -0300

    Fix: evitar importacion circular usando extensions.py para mail y jwt

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,9 @@
 from flask import Flask
-from flask_jwt_extended import JWTManager
 from flask_cors import CORS
-from flask_mail import Mail
 import mongoengine
 
 from config import Config
-
-jwt = JWTManager()
-mail = Mail()
+from extensions import jwt, mail
 
 def create_app():
     app = Flask(__name__)
